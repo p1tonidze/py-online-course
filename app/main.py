b08a1ea88184de95d2d5bb22dd83209a72a792c5
Yaroslav <104159378+y-havryliv@users.noreply.github.com>
2023-02-23 14:16:45 +0200

    Implement py-online-course

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
+from typing import Dict
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return (days + 6) // 7
+
+    @classmethod
+    def from_dict(cls, course_dict: Dict[str, str]) -> "OnlineCourse":
+        name = course_dict.get("name", "")
+        description = course_dict.get("description", "")
+        days = course_dict.get("days", 0)
+        weeks = cls.days_to_weeks(days)
+        return cls(name, description, weeks)
